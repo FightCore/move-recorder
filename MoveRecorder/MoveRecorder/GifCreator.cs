2acc077181fbd7d30cfcd785a51407a853232299
Bart de Bever <bartdebever112@gmail.com>
2023-03-31 11:29:02 +0200

    Create a gif from the gotten frames

---
+++ MoveRecorder/MoveRecorder/GifCreator.cs
@@ -0,0 +1,42 @@
+﻿using SixLabors.ImageSharp.Formats.Gif;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Image = SixLabors.ImageSharp.Image;
+
+namespace MoveRecorder;
+public class GifCreator
+{
+	public void Create(string fileName, List<Bitmap> frames, Screen screen)
+	{
+		// For the frame delay, it is usual to be around 5 FPS
+		const int frameDelay = 60 / 5;
+
+		var images = new List<Image>();
+		foreach (var frame in frames)
+		{
+			using var stream = new MemoryStream();
+			frame.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+			images.Add(Image.Load(stream.ToArray()));
+		}
+
+		var gif = images[0];
+
+		var gifMetaData = gif.Metadata.GetGifMetadata();
+		var metadata = gif.Frames.RootFrame.Metadata.GetGifMetadata();
+		metadata.FrameDelay = frameDelay;
+
+		foreach (var image in images.Skip(1))
+		{
+			metadata = image.Frames.RootFrame.Metadata.GetGifMetadata();
+			metadata.FrameDelay = frameDelay;
+
+			// Add the color image to the gif.
+			gif.Frames.AddFrame(image.Frames.RootFrame);
+		}
+
+		gif.SaveAsGif(fileName);
+	}
+}
--- MoveRecorder/MoveRecorder/Program.cs
+++ MoveRecorder/MoveRecorder/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 using System.Drawing.Imaging;
+using MoveRecorder;
+using Point = System.Drawing.Point;
 
 Console.WriteLine("Hello, World!");
 
@@ -30,6 +32,7 @@ var folder = $"{baseFolder}{character}/{move}/";
 
 Directory.CreateDirectory(folder);
 
+var frames = new List<Bitmap>();
 while (true)
 {
 	var line = Console.ReadLine();
@@ -38,14 +41,19 @@ while (true)
 		break;
 	}
 
-	using (var bitmap = new Bitmap(screenToUse.Bounds.Width, screenToUse.Bounds.Height))
+	var fileName = $"{folder}{frameCounter:D3}.png";
+	var bitmap = new Bitmap(screenToUse.Bounds.Width, screenToUse.Bounds.Height);
+	using (var g = Graphics.FromImage(bitmap))
 	{
-		using (var g = Graphics.FromImage(bitmap))
-		{
-			g.CopyFromScreen(new Point(screenToUse.Bounds.Left, screenToUse.Bounds.Top), Point.Empty, screenToUse.Bounds.Size);
-		}
-		bitmap.Save($"{folder}{frameCounter:D3}.png", ImageFormat.Png);
+		g.CopyFromScreen(new Point(screenToUse.Bounds.Left, screenToUse.Bounds.Top), Point.Empty, screenToUse.Bounds.Size);
 	}
+	bitmap.Save(fileName, ImageFormat.Png);
+
+	frames.Add(bitmap);
 	Console.WriteLine($"Taken screenshot of frame {frameCounter}");
 	frameCounter++;
 }
+
+Console.WriteLine("Saving as GIF");
+new GifCreator().Create($"{folder}.gif", frames, screenToUse);
+Console.WriteLine("Saved");
